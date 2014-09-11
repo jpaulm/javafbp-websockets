52d4b58d6fd3fae9639790329cccef7a208fec60
J Paul Morrison <jpaulmorr@gmail.com>
2014-09-11 15:16:58 -0400

    Initial commit for JavaFBP-WebSockets (minor bugs at close-down)

---
+++ src/main/java/com/jpmorrsn/fbp/examples/networks/TestWebSockets.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) J.P. Morrison, Enterprises, Ltd. 2009, 2014 All Rights Reserved. 
+ */
+package com.jpmorrsn.fbp.examples.networks;
+
+/* 
+ * Useful info:  http://stackoverflow.com/questions/18900187/processing-how-to-send-data-through-websockets-to-javascript-application
+ * 
+ * This project requires java_websocket.jar - obtainable from https://github.com/TooTallNate/Java-WebSocket/tree/master/dist
+ */
+
+
+import java.io.File;
+
+import com.jpmorrsn.fbp.components.WebSocketReceive;
+import com.jpmorrsn.fbp.components.WebSocketRespond;
+import com.jpmorrsn.fbp.engine.Network;
+import com.jpmorrsn.fbp.examples.components.WebSocketSimProc;
+
+
+/**
+ * This network uses the JavaFBP WebSocketReceive and WebSocketRespond components - these are basically 
+ * TooTallNate's AutobahnServerTest code, split into two processes -
+ * not to be confused with TestSockets, which uses WriteToSocket and ReadFromSocket! 
+ */
+
+public class TestWebSockets extends Network {
+
+  @Override
+  protected void define() {
+
+    component("WSRcv", WebSocketReceive.class);
+    component("Process", WebSocketSimProc.class);
+    component("WSRsp", WebSocketRespond.class);
+    
+    initialize(new Integer(9003), "WSRcv.PORT");
+
+    connect("WSRcv.OUT", "Process.IN", 40);
+    connect("Process.OUT", "WSRsp.IN", 40);
+    
+    initialize("lib" + File.separator + "javafbp-3.0.1.jar", "Process.JARFILE");
+  }
+
+  public static void main(final String[] argv) throws Exception {
+    Network net = new TestWebSockets();
+    //net.runTimeReqd = false;
+    net.go();
+  }
+
+}
