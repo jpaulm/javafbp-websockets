c816ad4a80e1992426356058aeda96cbd369846e
Paul Morrison <jpaulmorr@gmail.com>
2017-07-08 11:44:27 -0400

    Cleaned up test case; added RandomDelay to multiplexed paths; made multiplex factor a variable

--- src/main/java/com/jpaulmorrison/fbp/examples/networks/TestWebSockets.java
+++ src/main/java/com/jpaulmorrison/fbp/examples/networks/TestWebSockets.java
@@ -11,6 +11,7 @@ package com.jpaulmorrison.fbp.examples.networks;
 
 
 import com.jpaulmorrison.fbp.core.components.routing.LoadBalance;
+import com.jpaulmorrison.fbp.core.components.routing.RandomDelay;
 import com.jpaulmorrison.fbp.components.WebSocketReceive;
 import com.jpaulmorrison.fbp.components.WebSocketRespond;
 import com.jpaulmorrison.fbp.core.engine.Network;
@@ -26,28 +27,27 @@ import com.jpaulmorrison.fbp.examples.components.WebSocketSimProc;
 public class TestWebSockets extends Network {
 
   @Override
-  protected void define() {
+	protected void define() {
 
-    component("WSRcv", WebSocketReceive.class);
-    component("LBal", LoadBalance.class);
-    component("Process0", WebSocketSimProc.class);
-    component("WSRsp0", WebSocketRespond.class);
-    component("Process1", WebSocketSimProc.class);
-    component("WSRsp1", WebSocketRespond.class);
-    component("Process2", WebSocketSimProc.class);
-    component("WSRsp2", WebSocketRespond.class);
-    
-    initialize(new Integer(9003), "WSRcv.PORT");
+		int multiplexNo = 6;
+		component("WSRcv", WebSocketReceive.class);
+		component("LBal", LoadBalance.class);
+		for (int i = 0; i < multiplexNo; i++) {
+			component("Process" + i, WebSocketSimProc.class);
+			component("WSRsp" + i, WebSocketRespond.class);
+			component("RD" + i, RandomDelay.class);
+		}
 
-    connect("WSRcv.OUT", "LBal.IN", 4);    
-    connect("LBal.OUT[0]", "Process0.IN", 4);
-    connect("Process0.OUT", "WSRsp0.IN", 4);   
-    connect("LBal.OUT[1]", "Process1.IN", 4);
-    connect("Process1.OUT", "WSRsp1.IN", 4); 
-    connect("LBal.OUT[2]", "Process2.IN", 4);
-    connect("Process2.OUT", "WSRsp2.IN", 4); 
-    
-  }
+		initialize(new Integer(9003), "WSRcv.PORT");
+
+		connect("WSRcv.OUT", "LBal.IN", 4);
+		for (int i = 0; i < multiplexNo; i++) {
+			connect("LBal.OUT[" + i + "]", "RD" + i + ".IN", 4);
+			connect("RD" + i + ".OUT", "Process" + i + ".IN", 4);
+			connect("Process" + i + ".OUT", "WSRsp" + i + ".IN", 4);
+		}
+
+	}
 
   public static void main(final String[] argv) throws Exception {
     Network net = new TestWebSockets();
