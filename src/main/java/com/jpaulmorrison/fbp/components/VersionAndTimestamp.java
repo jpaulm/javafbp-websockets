11a9b4df48ea03b3397bf30ec2784880cd8d687f
Paul Morrison <jpaulmorr@gmail.com>
2020-10-21 10:25:03 -0400

    Update with files from build and publish﻿

--- src/main/java/com/jpaulmorrison/fbp/components/VersionAndTimestamp.java
+++ src/main/java/com/jpaulmorrison/fbp/components/VersionAndTimestamp.java
@@ -24,7 +24,7 @@ public final class VersionAndTimestamp {
 
   private static String version = "JavaFBP-WebSockets - version 1.2.5";
 
-  private static String date = "30 Sept., 2020";
+  private static String date = "Oct. 21, 2020";
 
   static String getVersion() {
     return version;
